2d440abd07729793f7cbb0305b5d1e0971fe8f86
zuri1 <zuri.biringer@gmail.com>
2013-11-14 09:58:26 -0800

    Added FlickrKit & JSONModel; parsed Tucker's photos with Flickr API

--- ZMBCocoaPodsAndNetworking/Pods/Pods-environment.h
+++ ZMBCocoaPodsAndNetworking/Pods/Pods-environment.h
@@ -6,12 +6,24 @@
 // project.
 
 
+// FlickrKit
+#define COCOAPODS_POD_AVAILABLE_FlickrKit
+#define COCOAPODS_VERSION_MAJOR_FlickrKit 1
+#define COCOAPODS_VERSION_MINOR_FlickrKit 0
+#define COCOAPODS_VERSION_PATCH_FlickrKit 2
+
 // JSONKit
 #define COCOAPODS_POD_AVAILABLE_JSONKit
 // This library does not follow semantic-versioning,
 // so we were not able to define version macros.
 // Please contact the author.
 // Version: 1.5pre.
+
+// JSONModel
+#define COCOAPODS_POD_AVAILABLE_JSONModel
+#define COCOAPODS_VERSION_MAJOR_JSONModel 0
+#define COCOAPODS_VERSION_MINOR_JSONModel 10
+#define COCOAPODS_VERSION_PATCH_JSONModel 0
 
 // Reachability
 #define COCOAPODS_POD_AVAILABLE_Reachability
--- ZMBCocoaPodsAndNetworking/ZMBCocoaPodsAndNetworking/ZMBViewController.m
+++ ZMBCocoaPodsAndNetworking/ZMBCocoaPodsAndNetworking/ZMBViewController.m
@@ -8,6 +8,8 @@
 
 #import "ZMBViewController.h"
 #import <JSONKit/JSONKit.h>
+#import <FlickrKit/FlickrKit.h>
+#import <JSONModel/JSONModel.h>
 
 @interface ZMBViewController ()
 
@@ -42,12 +44,49 @@
     _placeLabel.text = [weatherDict objectForKey:@"name"];
     _tempLabel.text = [NSString stringWithFormat:@"%@ K", [mainKeyDict objectForKey:@"temp"]];
     _weatherLabel.text = [weatherKeyDict objectForKey:@"description"];
+    
+    [[FlickrKit sharedFlickrKit] initializeWithAPIKey:@"c6506c0d2e8da8dadc72c4924f37828d" sharedSecret:@"a2bdae7dab12593d"];
+    
+    FlickrKit *fk = [FlickrKit sharedFlickrKit];
+    
+    NSData *flickrData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=f3dcd6e63a3a7cca3752fbe5939e1c59&user_id=42145244%40N03&format=json&nojsoncallback=1&auth_token=72157637669376345-0f985f7cdefec834&api_sig=56f44e1de244fbe2b8bd4489874a3b5b"]];
+    NSDictionary *flickrDict = [flickrData objectFromJSONData];
+    NSLog(@"%@", flickrDict);
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)refreshButtonPressed
+{
+    NSLog(@"Refresh button pressed");
+    
+    NSData *weatherData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://api.openweathermap.org/data/2.5/weather?q=everett,wa"]];
+    
+    NSDictionary *weatherDict = [weatherData objectFromJSONData];
+    
+    NSLog(@"%@", [weatherDict objectForKey:@"weather"]);
+    NSLog(@"%@", [weatherDict objectForKey:@"main"]);
+    
+    NSArray *weatherKeyArray = [weatherDict objectForKey:@"weather"];
+    NSLog(@"%@", [weatherKeyArray objectAtIndex:0]);
+    
+    NSDictionary *weatherKeyDict = [weatherKeyArray objectAtIndex:0];
+    NSLog(@"%@", [weatherKeyDict objectForKey:@"description"]);
+    
+    NSDictionary *mainKeyDict = [weatherDict objectForKey:@"main"];
+    NSLog(@"%@", [mainKeyDict objectForKey:@"temp"]);
+    //NSLog(@"%@", [weatherDict objectForKey:@"main"]);
+    
+    NSLog(@"%@", [weatherDict objectForKey:@"name"]);
+    
+    _placeLabel.text = [weatherDict objectForKey:@"name"];
+    _tempLabel.text = [NSString stringWithFormat:@"%@ K", [mainKeyDict objectForKey:@"temp"]];
+    _weatherLabel.text = [weatherKeyDict objectForKey:@"description"];
 }
 
 @end
